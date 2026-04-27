da4dc4c2a52262c5f2e7b681d86923d605f4bdcc
Yassir0123 <mzakyassir@gmail.com>
2026-04-27 21:45:17 +0100

    set

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const app = express();
 
 app.use(express.json());
 
+// test route (IMPORTANT)
+app.get("/", (req, res) => {
+  res.send("alive");
+});
+
 // your webhook route
 app.post("/webhook", (req, res) => {
   console.log("Received:", req.body);
   res.sendStatus(200);
 });
 
-// IMPORTANT: Railway uses dynamic ports
+// dynamic port (already correct)
 const PORT = process.env.PORT || 5555;
 
 app.listen(PORT, () => {
